e7ee3d835c75ffb2e3d85143e2f3e5b142791117
valentinpalade28 <valentinpalade28@gmail.com>
2018-10-30 16:09:27 +0200

    Add files via upload

---
+++ tama/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Tama</title>
+    <meta charset="UTF-8">
+  </head>
+  <link rel="stylesheet" type="text/css" href="assets/css/style-teodor.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+<body id="sign-in-page">
+
+  <main>
+    <img alt="Tama Bunny" src="assets/images/tama-bunny.png">
+    <form>
+      <label for="name">Name</label><br>
+      <input type="text" id="name" name="name"><br>
+      <label for="pass">Password</label><br>
+      <input type="password" id="pass" name="pass"><br><br>
+      <input type="submit" value="Sign In">
+      <a href="sign-up.php">Sign Up</a>
+    </form>
+  </main>
+
+</body>
+</html>
