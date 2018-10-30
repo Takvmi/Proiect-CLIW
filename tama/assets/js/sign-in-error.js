2b4faba7b43321b8d76309655e6475ea5d845025
Tanase Teodor <DoubleBlueT@gmail.com>
2018-10-30 19:56:22 +0200

    New iteration of the Sign In page.
    Added vector graphics for the sign in page.
    Remade the sign in system so that it would work.
    Separated the javascript files in a new folder.

---
+++ tama/assets/js/sign-in-error.js
@@ -0,0 +1,54 @@
+function errorJS(){
+				
+			var error = null;
+			var success = null;
+			
+			name = document.getElementById('name').value;
+			pass = document.getElementById('pass').value;
+	
+			xhr = new XMLHttpRequest();
+
+			xhr.open('POST', 'signIn.php');
+			xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+			xhr.onload = function() {
+			if (xhr.status !== 200) {
+				alert(' Request failed.  Returned status of ' + xhr.status + '\n Stop spamming that button!');
+			}
+			else
+			{
+			returnData = JSON.parse(xhr.responseText);
+			error = returnData.error;	
+			success = returnData.success;
+			
+			var vUrl = window.location.href;
+			if(error == 11){
+			document.getElementById("nameErrorDiv").innerHTML="<p class='cError'>Name field is empty.</p>";
+			document.getElementById("passErrorDiv").innerHTML="<br><br>";
+			} else if (error == 12){
+				document.getElementById("nameErrorDiv").innerHTML="<p class='cError'>User does not exist.</p>";
+				document.getElementById("passErrorDiv").innerHTML="<br><br>";
+			} else if (error == 21){
+				document.getElementById("passErrorDiv").innerHTML="<p class='cError'>Password field is empty.</p>";
+				document.getElementById("nameErrorDiv").innerHTML="<br><br>";
+			} else if (error == 22){
+				document.getElementById("passErrorDiv").innerHTML="<p class='cError'>Password is wrong.</p>";
+				document.getElementById("nameErrorDiv").innerHTML="<br><br>";
+			}
+			else if (error > 30)
+			{
+				document.getElementById("passErrorDiv").innerHTML="<p class='cError'>Unknown error.</p>";
+				document.getElementById("nameErrorDiv").innerHTML="<p class='cError'>Unknown error.</p>";
+			}
+			else if (error == 0 && success == 1)
+			{
+				window.location.replace("http://localhost/Project_TAMA/pet-home.php");
+			}
+				
+			}
+			}
+			xhr.send(encodeURI('name=' + name + '&pass=' + pass));
+};
+
+function signUPJS(){
+	window.location.replace("http://localhost/Project_TAMA/sign-up.php");
+};
--- tama/index.php
+++ tama/index.php
@@ -1,23 +1,32 @@
-<!DOCTYPE html>
+<?php
+   session_start();
+?>
 <html>
   <head>
     <title>Tama</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style-teodor.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style-index.css">
     <meta charset="UTF-8">
   </head>
-  <link rel="stylesheet" type="text/css" href="assets/css/style-teodor.css">
-  <link rel="stylesheet" type="text/css" href="assets/css/main.css">
 <body id="sign-in-page">
 
   <main>
-    <img alt="Tama Bunny" src="assets/images/tama-bunny.png">
-    <form>
+    <img alt="Tama Bunny" src="assets/images/tama-bunny.svg">
+	<form>
       <label for="name">Name</label><br>
       <input type="text" id="name" name="name"><br>
-      <label for="pass">Password</label><br>
-      <input type="password" id="pass" name="pass"><br><br>
-      <input type="submit" value="Sign In">
-      <a href="sign-up.php">Sign Up</a>
+		 <div id="nameErrorDiv"><br><br></div>
+		 <script src="assets/js/sign-in-error.js"></script>
+	  <label for="pass">Password</label><br>
+      <input type="password" id="pass" name="pass"><br>
+		 <div id="passErrorDiv"><br><br></div>
+		 <div id="extraErrorDiv"></div>
     </form>
+	<div style="align: center;">
+	<div class="cButton" style="width:220px; padding: 40px; padding-top: 0px; ">
+	<button style="width:220px; position:relative;" onclick="errorJS()">Sign In</button>
+	<button style="width:220px; position:relative;" onclick="signUPJS()">Sign Up</button>
+	</div>
   </main>
 
 </body>
