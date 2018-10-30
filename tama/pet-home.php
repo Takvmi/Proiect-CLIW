59de20dd92d19b003b0cf030c8b9873d246787d5
valentinpalade28 <valentinpalade28@gmail.com>
2018-10-30 20:40:32 +0200

    Pet Home Page Header

--- tama/pet-home.php
+++ tama/pet-home.php
@@ -1,1 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Tama</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="assets/css/style-voli.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+  </head>
 
+  <body id="pet-home-page">
+    <header>
+      <ul>
+        <li>Level: 13</li>
+        <li>Points: 42</li>
+      </ul>
+      <button>Sign Out</button>
+      <nav>
+        <a href="pet-create.php">Create Pet</a>
+        <a href="pet-select.php">Select Pet</a>
+      </nav>
+    </header>
+    <main>
+      <!-- <img alt="Tama Bunny" src="assets/images/tama-bunny.png"> -->
+
+    </main>
+
+  </body>
+</html>
