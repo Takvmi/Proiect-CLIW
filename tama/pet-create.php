f15673429f16d46e0f8ef2c0491a2e262bee5813
valentinpalade28 <valentinpalade28@gmail.com>
2018-10-31 16:13:47 +0200

    Pet Create Page

--- tama/pet-create.php
+++ tama/pet-create.php
@@ -19,35 +19,78 @@
       </div>
     </main>
     <aside id="left-side">
-      <div class="body-part-container">
-        <img alt="Tama Bunny Head" src="assets/images/tama-bunny-head.svg">
+      <div id="select-buttons">
+        <button>H</button>
+        <button>B</button>
+        <button>T</button>
       </div>
-      <div class="body-part-container">
-        <img alt="Tama Cat Head" src="assets/images/tama-cat-head.svg">
-      </div>
-      <div class="body-part-container">
-        <img alt="Tama Bunny Arms" src="assets/images/tama-bunny-arms.svg">
-      </div>
-      <div class="body-part-container">
-        <img alt="Tama Cat Arms" src="assets/images/tama-cat-arms.svg">
-      </div>
-      <div class="body-part-container">
-        <img alt="Tama Bunny Body" src="assets/images/tama-bunny-body.svg">
-      </div>
-      <div class="body-part-container">
-        <img alt="Tama Cat Body" src="assets/images/tama-cat-body.svg">
+      <div id="body-parts-container">
+        <div class="body-part">
+          <img alt="Tama Bunny Head" src="assets/images/tama-bunny-head-preview.svg">
+          <span>5</span>
+        </div>
+        <div class="body-part">
+          <img alt="Tama Cat Head" src="assets/images/tama-cat-head-preview.svg">
+          <span>14</span>
+        </div>
+        <div class="body-part">
+          <img alt="Tama Skeleton Head" src="assets/images/tama-skeleton-head-preview.svg">
+          <span>14</span>
+        </div>
+        <div class="body-part">
+          <img alt="Tama Bunny Body" src="assets/images/tama-bunny-body-preview.svg">
+          <span>10</span>
+        </div>
+        <div class="body-part">
+          <img alt="Tama Cat Body" src="assets/images/tama-cat-body-preview.svg">
+          <span>5</span>
+        </div>
+        <div class="body-part">
+          <img alt="Tama Skeleton Body" src="assets/images/tama-skeleton-body-preview.svg">
+          <span>14</span>
+        </div>
+        <div class="body-part">
+          <img alt="Tama Bunny Toy" src="assets/images/toy-bunny.svg">
+          <span>10</span>
+        </div>
+        <div class="body-part">
+          <img alt="Tama Cat Toy" src="assets/images/toy-yarn.svg">
+          <span>5</span>
+        </div>
+        <div class="body-part">
+          <img alt="Tama Skeleton Toy" src="assets/images/toy-pumpkin.svg">
+          <span>14</span>
+        </div>
       </div>
     </aside>
     <aside id="right-side">
-      <div class="stats-bar-container">
-        <div id="feed-stats-bar" class="stats-bar" style="width:25%">25%</div>
-      </div>
-      <div class="stats-bar-container">
-        <div id="educate-stats-bar" class="stats-bar" style="width:50%">50%</div>
-      </div>
-      <div class="stats-bar-container">
-        <div id="entertain-stats-bar" class="stats-bar" style="width:75%">75%</div>
-      </div>
+      <table>
+        <tbody>
+          <tr>
+            <th>Hunger</th>
+            <th>:</th>
+            <th>19</th>
+          </tr>
+          <tr>
+            <th>Education</th>
+            <th>:</th>
+            <th>5</th>
+          </tr>
+          <tr>
+            <th>Fun</th>
+            <th>:</th>
+            <th>0</th>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <th>Cost</th>
+            <th>:</th>
+            <th>24</th>
+          </tr>
+        </tfoot>
+      </table>
+      <button>Create</button>
     </aside>
   </body>
 </html>
