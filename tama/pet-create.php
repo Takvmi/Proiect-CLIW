a30c7d8759e8e25a3068cd3fd20d4f516e316911
valentinpalade28 <valentinpalade28@gmail.com>
2018-10-31 13:56:04 +0200

    Pet Create Page

--- tama/pet-create.php
+++ tama/pet-create.php
@@ -1,1 +1,53 @@
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Tama</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="assets/css/style-voli.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+  </head>
+  <body id="pet-create-page">
+    <header>
+      <a href="pet-home.php">Back</a>
+      <div class="available-points">Points: 42</div>
+      <button>Sign Out</button>
+    </header>
+    <main>
+      <div id="main-content">
+        <img alt="Tama Bunny" src="assets/images/tama-bunny.svg">
+        <input type="text" id="pet-name" name="pet-name" placeholder="Enter Pet Name">
+      </div>
+    </main>
+    <aside id="left-side">
+      <div class="body-part-container">
+        <img alt="Tama Bunny Head" src="assets/images/tama-bunny-head.svg">
+      </div>
+      <div class="body-part-container">
+        <img alt="Tama Cat Head" src="assets/images/tama-cat-head.svg">
+      </div>
+      <div class="body-part-container">
+        <img alt="Tama Bunny Arms" src="assets/images/tama-bunny-arms.svg">
+      </div>
+      <div class="body-part-container">
+        <img alt="Tama Cat Arms" src="assets/images/tama-cat-arms.svg">
+      </div>
+      <div class="body-part-container">
+        <img alt="Tama Bunny Body" src="assets/images/tama-bunny-body.svg">
+      </div>
+      <div class="body-part-container">
+        <img alt="Tama Cat Body" src="assets/images/tama-cat-body.svg">
+      </div>
+    </aside>
+    <aside id="right-side">
+      <div class="stats-bar-container">
+        <div id="feed-stats-bar" class="stats-bar" style="width:25%">25%</div>
+      </div>
+      <div class="stats-bar-container">
+        <div id="educate-stats-bar" class="stats-bar" style="width:50%">50%</div>
+      </div>
+      <div class="stats-bar-container">
+        <div id="entertain-stats-bar" class="stats-bar" style="width:75%">75%</div>
+      </div>
+    </aside>
+  </body>
+</html>
